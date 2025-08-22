630f64ababa8104e4c79cffea72440d4e2ec3a8c
proximussss <rapooboitumelo2@gmail.com>
2025-08-22 15:00:43 +0200

    supabase configs

---
+++ src/services/api/supabase.js
@@ -0,0 +1,9 @@
+// server/supabase.js
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY // ⚠️ service key, never expose to frontend
+);
+
+module.exports = supabase;
