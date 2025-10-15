72f4aa48f1a364db57e4e89ec3318d73ad87425f
Banele <mkhizeb174@gmail.com>
2025-10-15 14:31:48 +0200

    images and tsx files for index page

--- Lance-Sports/src/main.tsx
+++ Lance-Sports/src/main.tsx
@@ -1,9 +1,10 @@
   import { createRoot } from "react-dom/client";
-  import App from "./components/App.tsx";
-  import "./index.css";
+  import App from "./components/App";
+  import IntroPage from "./components/index";
+  import "./index.css"; 
   import { GoogleOAuthProvider } from "@react-oauth/google";
 
   createRoot(document.getElementById("root")!).render( <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-          <App />
+          <IntroPage />
         </GoogleOAuthProvider>);
   
