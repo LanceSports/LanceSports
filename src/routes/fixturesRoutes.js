10ae7e3071cf18da47bc154f633bb25de517b7a4
Thuthu-K <Thuthusbu10@gmail.com>
2025-10-18 13:15:02 +0200

    exported convert BigInts from fixturesroutes.js so can use in logoddsroutes

--- src/routes/fixturesRoutes.js
+++ src/routes/fixturesRoutes.js
@@ -7,7 +7,7 @@ import { delay } from "../utils/rateLimit.js";
 const router = express.Router();
 
 // Helper to safely stringify BigInts
-function convertBigInts(obj) {
+export async function convertBigInts(obj) {
   if (typeof obj === "bigint") return obj.toString();
   if (Array.isArray(obj)) return obj.map(convertBigInts);
   if (obj !== null && typeof obj === "object") {
